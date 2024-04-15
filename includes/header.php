2a99b9f3b0aeb8987101b06d9425d358fd0f99c6
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2024-04-15 17:52:41 +0545

    Initial commit

---
+++ includes/header.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Real State Nepal</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+
+<body>
+    <nav class="main-nav">
+        <div class="wrapper">
+            <a href="#" class="brand-name">GharJagga</a>
+            <ul class="menu-list">
+                <li class="menu-item"><a href="#" class="menu-link">House</a></li>
+                <li class="menu-item"><a href="#" class="menu-link">Land</a></li>
+                <li class="menu-item"><a href="#" class="menu-link">Rent</a></li>
+                <li class="menu-item"><a href="#" class="menu-link">Blog</a></li>
+            </ul>
+            <ul class="menu-list secondary-list">
+                <li class="menu-item">
+                    <a href="#" class="menu-link">
+                        <button type="button" class="btn-primary">
+                            <span class="label-text">
+                                Post Property
+                            </span>
+                        </button>
+                    </a>
+                </li>
+                <li class="menu-item">
+                    <a href="#" class="menu-link">
+                        <button type="button" class="btn-secondary">
+                            <span class="icon-container">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                    <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+                                </svg>
+                            </span>
+                            <span class="label-text">
+                                Login
+                            </span>
+                        </button>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </nav>
---
+++ index.php
@@ -0,0 +1,11 @@
+<?php include './includes/header.php' ?>
+
+
+<footer class="main-footer">
+    
+</footer>
+
+
+</body>
+
+</html>
