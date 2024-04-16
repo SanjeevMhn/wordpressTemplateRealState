73c89f66b7848b5669d97a2e6c9e5483af94b248
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2024-04-16 18:24:53 +0545

    [ADDED]: changed font and added content in the frontpage

--- index.php
+++ index.php
@@ -1,4 +1,13 @@
 <?php include './includes/header.php' ?>
 
+    <div class="wrapper">
+        <div class="intro-section">
+            <div class="intro-text">
+                <h2 class="heading-text">Searching For A Property?</h2>
+                <h2 class="heading-text">Find it in Gharjagga.</h2>
+                <span class="sub-heading-text">Discover a wide range of properties for sale or rent in your desired location.</span>
+            </div>
+        </div>
+    </div>
 
 <?php include "./includes/footer.php" ?>
