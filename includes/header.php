e4ce2735b366a32f5f17a45f7f0ccf46760b7bd1
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2024-04-17 18:13:18 +0545

    [ADDED]: added intro page

--- includes/header.php
+++ includes/header.php
@@ -8,6 +8,9 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,opsz,wght@0,6..12,200..1000;1,6..12,200..1000&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 
@@ -22,7 +25,7 @@
                 <li class="menu-item"><a href="#" class="menu-link">Blog</a></li>
             </ul>
             <ul class="menu-list secondary-list">
-                <li class="menu-item">
+                <!-- <li class="menu-item">
                     <a href="#" class="menu-link">
                         <button type="button" class="btn-primary">
                             <span class="label-text">
@@ -30,19 +33,17 @@
                             </span>
                         </button>
                     </a>
-                </li>
+                </li> -->
                 <li class="menu-item">
                     <a href="#" class="menu-link">
-                        <button type="button" class="btn-secondary">
-                            <span class="icon-container">
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                                    <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
-                                </svg>
-                            </span>
-                            <span class="label-text">
-                                Login
-                            </span>
-                        </button>
+                        <span class="icon-container">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                                <path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z" />
+                            </svg>
+                        </span>
+                        <span class="label-text">
+                            Login
+                        </span>
                     </a>
                 </li>
             </ul>
--- index.php
+++ index.php
@@ -1,13 +1,25 @@
 <?php include './includes/header.php' ?>
 
+<div class="intro-section">
     <div class="wrapper">
-        <div class="intro-section">
-            <div class="intro-text">
-                <h2 class="heading-text">Searching For A Property?</h2>
-                <h2 class="heading-text">Find it in Gharjagga.</h2>
-                <span class="sub-heading-text">Discover a wide range of properties for sale or rent in your desired location.</span>
+        <div class="intro-text">
+            <h2>Searching For A Property?</h2>
+            <h4>Find it in Gharjagga.</h4>
+            <span>Discover a wide range of properties for sale or rent in your desired location.</span>
+            <div class="btn-group">
+                <a href="#" class="btn-primary property-btn">
+                    Search Property
+                </a>
+                <button type="button" class="btn-primary-outline property-btn">
+                    Post Property
+                </button>
             </div>
         </div>
+        <div class="hero-img">
+            <img src="./assets/images/hero.jpg" alt="">
+        </div>
     </div>
+
+</div>
 
 <?php include "./includes/footer.php" ?>
