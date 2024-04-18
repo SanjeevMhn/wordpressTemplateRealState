130ca7645208427bbe15ead2515646fc0ab13201
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2024-04-18 18:22:22 +0545

    [ADDED]: added product cards

--- index.php
+++ index.php
@@ -19,7 +19,51 @@
             <img src="./assets/images/hero.jpg" alt="">
         </div>
     </div>
-
 </div>
+<section class="product-list-container">
+    <div class="wrapper">
+        <div class="header-content">
+            <h2 class="title-text">Housing & Apartments</h2>
+            <a href="#" class="">View All</a>
+        </div>
+        <ul class="product-list">
+            <li class="list-item">
+                <a href="#" class="list-link product-card">
+                    <div class="img-container">
+                        <img src="./assets/images/prop_1.jpg" alt="">
+                    </div>
+                </a>
+            </li>
+            <li class="list-item">
+                <a href="#" class="list-link product-card">
+                    <div class="img-container">
+                        <img src="./assets/images/prop_2.jpg" alt="">
+                    </div>
+                </a>
+            </li>
+            <li class="list-item">
+                <a href="#" class="list-link product-card">
+                    <div class="img-container">
+                        <img src="./assets/images/prop_3.jpg" alt="">
+                    </div>
+                </a>
+            </li>
+            <li class="list-item">
+                <a href="#" class="list-link product-card">
+                    <div class="img-container">
+                        <img src="./assets/images/prop_4.jpg" alt="">
+                    </div>
+                </a>
+            </li>
+            <li class="list-item">
+                <a href="#" class="list-link product-card">
+                    <div class="img-container">
+                        <img src="./assets/images/prop_5.jpg" alt="">
+                    </div>
+                </a>
+            </li>
+        </ul>
+    </div>
+</section>
 
 <?php include "./includes/footer.php" ?>
