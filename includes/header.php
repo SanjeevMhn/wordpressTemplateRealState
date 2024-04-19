2f4e9cd5dbc11d1b9777d778bc597efad1d6ccc1
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2024-04-19 18:22:02 +0545

    [ADDED]: added responsive css

--- includes/header.php
+++ includes/header.php
@@ -18,6 +18,11 @@
     <nav class="main-nav">
         <div class="wrapper">
             <a href="#" class="brand-name heading-text">GharJagga</a>
+            <button class="resp-menu-btn">
+                <span class="icon-container">
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z"/></svg>
+                </span>
+            </button>
             <ul class="menu-list">
                 <li class="menu-item"><a href="#" class="menu-link">House</a></li>
                 <li class="menu-item"><a href="#" class="menu-link">Land</a></li>
--- index.php
+++ index.php
@@ -4,15 +4,12 @@
     <div class="wrapper">
         <div class="intro-text">
             <h2>Searching For A Property?</h2>
-            <h4>Find it in Gharjagga.</h4>
+            <h4>Find it here at <div class="brand-name">Gharjagga</div></h4>
             <span>Discover a wide range of properties for sale or rent in your desired location.</span>
             <div class="btn-group">
                 <a href="#" class="btn-primary property-btn">
                     Search Property
                 </a>
-                <button type="button" class="btn-primary-outline property-btn">
-                    Post Property
-                </button>
             </div>
         </div>
         <div class="hero-img">
